7375c14cc676e17492e138ae4bb519a19f52d859
Saksham Kothari <109241331+Saksham-Kothari@users.noreply.github.com>
2024-07-12 20:34:09 +0530

    Time: 20 ms (100%), Space: 45.3 MB (79.8%) - LeetHub

---
+++ 1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int maximumGain(String s, int x, int y) {
+        int countA = 0;
+        int countB = 0;
+        int res = 0;
+        int less = Math.min(x,y);
+        for(int i=0;i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch>'b'){
+                res+=Math.min(countA, countB)*less;
+                countA=0;
+                countB=0;
+            }else if (ch =='a'){
+                if (x<y && countB >0){
+                    countB--;
+                    res+=y;
+                }else{
+                    countA++;
+                }
+            }else{
+                if(x>y && countA>0){
+                    countA--;
+                    res+=x;
+                }else{
+                    countB++;
+                }
+            }
+        }
+
+        res+= Math.min(countA,countB)*less;
+        return res;
+    }
+
+}
